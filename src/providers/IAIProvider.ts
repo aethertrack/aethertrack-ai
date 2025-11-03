bba22f4cca688b40ff959ece47261216664acba8
stormyoasis <goprotege@gmail.com>
2025-11-03 10:29:31 -0800

    ATAI-2: Defined support interfaces for provider interface

--- src/providers/IAIProvider.ts
+++ src/providers/IAIProvider.ts
@@ -1,3 +1,5 @@
+import type { IAIGenerationConfig } from "../types/IAIGenerationConfig";
+import type { IAIProviderConfig } from "../types/IAIProviderConfig";
 
 /**
  * Enumeration of supported AI Provider types
@@ -15,10 +17,11 @@ export interface IAIProvider {
     type: AIProviderType; // Type of provider (ie: openai, anthropic, etc.)
     
     /** Initialize the provider with necessary configuration
+     * 
      * @param config Configuration object specific to the provider
      * @returns Promise that resolves when initialization is complete
      */     
-    init(config: any): Promise<void>;
+    init(config: IAIProviderConfig): Promise<void>;
 
     /**
      * The core text generation function. Every provider must support this
@@ -27,5 +30,5 @@ export interface IAIProvider {
      * @param options Optional parameters specific to the provider/model
      * @returns Promise resolving to the generated response object
      */
-    generateText(prompt: string, options?: any): Promise<any>;
+    generateText(prompt: string, options?: IAIGenerationConfig): Promise<any>;
 }
